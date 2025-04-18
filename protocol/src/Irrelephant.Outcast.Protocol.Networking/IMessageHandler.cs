a19ca6321bef87683078afe4194eefcc3b4a1741
Eugene 'marengo_hue' Rebedailo <marengohue@irrelephant.io>
2025-04-18 02:09:13 +0200

    Extract message handler

---
+++ protocol/src/Irrelephant.Outcast.Protocol.Networking/IMessageHandler.cs
@@ -0,0 +1,17 @@
+﻿using Irrelephant.Outcast.Protocol.Abstractions.DataTransfer.Messages;
+using Irrelephant.Outcast.Protocol.Networking.EventModel;
+
+namespace Irrelephant.Outcast.Protocol.Networking;
+
+public interface IMessageHandler
+{
+    public DateTimeOffset LastNetworkActivity { get; }
+
+    event EventHandler<Message>? InboundMessageReceived;
+
+    event EventHandler<OutboundMessageAvailableArgs>? OutboundMessageAvailable;
+
+    void ProcessRead(Memory<byte> buffer, int receivedBytes, bool isAsync = true);
+    void EnqueueMessage(Message message);
+    void Reset();
+}
