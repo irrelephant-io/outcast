78f778c2793673b4ea238cf85b1e53ba47a60d54
Eugene 'marengo_hue' Rebedailo <marengohue@irrelephant.io>
2025-05-18 22:56:33 +0200

    Add character attribute notifications

---
+++ networking/src/Irrelephant.Outcast.Networking.Protocol.Abstractions/DataTransfer/Messages/CharacterAttributeNotifications.cs
@@ -0,0 +1,11 @@
+﻿namespace Irrelephant.Outcast.Networking.Protocol.Abstractions.DataTransfer.Messages;
+
+public record MaxHealthNotification(
+    Guid EntityId,
+    int MaxHealth
+) : Message;
+
+public record MovementSpeedNotification(
+    Guid EntityId,
+    float MovementSpeed
+) : Message;
